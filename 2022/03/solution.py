b801a39b9a149c97de70bcb07bbef7baaa9ddd50
Tapani Kiiskinen <tapani.kiiskinen@gmail.com>
2022-12-18 02:27:33 +0200

    2022 Day 3

--- 2022/03/solution.py
+++ 2022/03/solution.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
-
-
-
-
 with open("input.txt") as f:
     lines = f.read().splitlines()
+    rucksacks = [(line[:len(line) // 2], line[len(line) // 2:]) for line in lines]
 
-print()
+def char_priority(char: str) -> int:
+    if ord(char) < ord("a"):
+        return ord(char) - ord("A") + 27
+    return ord(char) - ord("a") + 1
+
+print(sum(char_priority((set(first) & set(second)).pop()) for (first, second) in rucksacks))
+
+def grouper(iterable, n):
+    args = [iter(iterable)] * n
+    return zip(*args, strict=True)
+
+print(sum(char_priority((set(first) & set(second) & set(third)).pop()) for (first, second, third) in grouper(lines, 3)))
