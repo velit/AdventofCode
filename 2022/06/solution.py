83ebef11939830665583f573df3b44d7a830b6e9
Tapani Kiiskinen <tapani.kiiskinen@gmail.com>
2022-12-24 21:27:55 +0200

    2022 Day 6

--- 2022/06/solution.py
+++ 2022/06/solution.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
-
-
-
-
 with open("input.txt") as f:
-    lines = f.read().splitlines()
+    cypher = f.read().splitlines()[0]
 
-print()
+def find_unique_sequence(cypher: str, length: int):
+    for i in range(len(cypher) - length - 1):
+        window = cypher[i:i + length]
+        if len(set(window)) == length:
+            print(i + length)
+            break
+    else:
+        print("Not found")
+
+find_unique_sequence(cypher, 4)
+find_unique_sequence(cypher, 14)
