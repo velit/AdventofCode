974c20d7eb36a2a3aea5176e00afbd7bcc102d27
Tapani Kiiskinen <tapani.kiiskinen@gmail.com>
2023-03-03 12:16:23 +0200

    2022 Day 9

--- 2022/09/solution.py
+++ 2022/09/solution.py
@@ -1,10 +1,59 @@
 #!/usr/bin/env python3
+from __future__ import annotations
+
+from collections.abc import Iterable
+from dataclasses import dataclass
 
 
+@dataclass(frozen=True)
+class Coord:
+    y: int = 0
+    x: int = 0
 
+    def __add__(self, other: Coord) -> Coord:
+        return Coord(self.y + other.y, self.x + other.x)
 
+directions = {
+    "D": Coord(+1, +0),
+    "R": Coord(+0, +1),
+    "U": Coord(-1, +0),
+    "L": Coord(+0, -1),
+}
+
+def sign(n: int) -> int:
+    if n > 0:
+        return 1
+    elif n < 0:
+        return -1
+    return 0
 
 with open("input.txt") as f:
     lines = f.read().splitlines()
+    instructions = [(directions[dir_], int(count)) for dir_, count in (line.split() for line in lines)]
 
-print()
+def simulate_rope(instructions: Iterable[tuple[Coord, int]], length: int) -> int:
+    rope = [Coord() for _ in range(length)]
+    visited = {rope[-1]}
+
+    for direction, count in instructions:
+        for _ in range(count):
+            rope[0] += direction
+
+            for i in range(1, length):
+                y_diff = rope[i - 1].y - rope[i].y
+                x_diff = rope[i - 1].x - rope[i].x
+
+                if abs(y_diff) < 2 and abs(x_diff) < 2:
+                    y_diff, x_diff = 0, 0
+                else:
+                    if abs(y_diff) == 2:
+                        y_diff -= sign(y_diff)  # reduce 1 from magnitude
+                    if abs(x_diff) == 2:
+                        x_diff -= sign(x_diff)
+
+                rope[i] += Coord(y_diff, x_diff)
+            visited.add(rope[-1])
+    return len(visited)
+
+print(simulate_rope(instructions, 2))
+print(simulate_rope(instructions, 10))
