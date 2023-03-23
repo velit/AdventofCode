ff88dc9408f413a8f840b39f18d7fec1fd4f697a
Tapani Kiiskinen <tapani.kiiskinen@gmail.com>
2023-03-23 14:26:40 +0200

    2022 Day 18

--- 2022/18/solution.py
+++ 2022/18/solution.py
@@ -1,8 +1,70 @@
 #!/usr/bin/env python3
+from collections.abc import Iterable
+from dataclasses import field, dataclass
+from itertools import product
+
+from common.coord import Position
+
+@dataclass
+class Mountain:
+    unevaluated: set[Position] = field(default_factory=lambda: set(Position(*pos) for pos in product(*[range(20)] * 3)))
+    cubes: set[Position] = field(default_factory=set)
+    faces = 0
+
+    def fill_cube(self, cube: Position):
+        cube_neighbours = neighbour_amount(self.cubes, cube)
+        self.faces += 6 - cube_neighbours * 2
+        self.cubes.add(cube)
+        self.unevaluated.remove(cube)
+
+neighbour_positions = (
+    Position(-1, 0, 0),
+    Position(0, -1, 0),
+    Position(0, 0, -1),
+    Position(0, 0, 1),
+    Position(0, 1, 0),
+    Position(1, 0, 0),
+)
+
+def neighbours(origin: Position) -> Iterable[Position]:
+    for neighbour_direction in neighbour_positions:
+        yield origin + neighbour_direction
+
+def neighbour_amount(positions: set[Position], origin: Position) -> int:
+    return sum(1 for neighbour in neighbours(origin) if neighbour in positions)
 
 def main() -> None:
     with open("input.txt") as f:
-        lines = f.read().splitlines()
+        cube_list = [Position(int(x), int(y), int(z)) for x, y, z in (line.split(",") for line in f.read().splitlines())]
+    m = Mountain()
+    for cube in cube_list:
+        m.fill_cube(cube)
+
+    # part 1
+    print(m.faces)
+
+    while m.unevaluated:
+        stack = [next(iter(m.unevaluated))]
+        current_set = {stack[0]}
+        inside = True
+
+        while stack:
+            current = stack.pop()
+            empty_neighbours = (pos for pos in neighbours(current) if pos not in current_set and pos not in m.cubes)
+            for neighbor in empty_neighbours:
+                if neighbor not in m.unevaluated:
+                    inside = False
+                    continue
+                current_set.add(neighbor)
+                stack.append(neighbor)
+
+        if inside:
+            for cube in current_set:
+                m.fill_cube(cube)
+        m.unevaluated -= current_set
+
+    # part 2
+    print(m.faces)
 
 if __name__ == "__main__":
     main()
--- 2022/common/coord.py
+++ 2022/common/coord.py
@@ -23,3 +23,27 @@ class Coord:
         if key == 1:
             return self.x
         raise IndexError(f"Invalid {key=}")
+
+@dataclass(order=True, frozen=True)
+class Position:
+    x: int = 0
+    y: int = 0
+    z: int = 0
+
+    def __add__(self, other: Position) -> Position:
+        return Position(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other: Position) -> Position:
+        return Position(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __len__(self):
+        return 3
+
+    def __getitem__(self, key):
+        if key == 0:
+            return self.x
+        if key == 1:
+            return self.y
+        if key == 2:
+            return self.z
+        raise IndexError(f"Invalid {key=}")
